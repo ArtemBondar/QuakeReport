c01aa77a90b9c1f29c0844b80e5a619e6af73beb
Artem Bondar <bondaraw@gmail.com>
2016-09-03 00:37:23 +0300

    added custom ListView, ArrayAdapter and class that describe earthquake;

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,42 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter(Context context, List<Earthquake> earthquakes) {
+        super(context, 0, earthquakes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.earthquake_list_item, parent, false);
+        }
+
+        Earthquake earthquake = getItem(position);
+
+        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        magnitudeView.setText(earthquake.getMagnitude());
+
+        TextView locationView = (TextView) listItemView.findViewById(R.id.location);
+        locationView.setText(earthquake.getLocation());
+
+
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        dateView.setText(earthquake.getDate());
+
+
+        return listItemView;
+    }
+}
