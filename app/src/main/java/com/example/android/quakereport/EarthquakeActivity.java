e5e2de9e0416c2efd2820fd880659f46b6943d7c
Artem Bondar <bondaraw@gmail.com>
2016-09-07 23:12:16 +0300

    Added ProgressBar and TextView to represent no earthquake data and no Internet connection

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,8 @@ package com.example.android.quakereport;
 
 import android.app.LoaderManager;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.content.*;
@@ -24,16 +26,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.android.quakereport.loader.EarthquakeLoader;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
-    private static final String BASE_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&limit=15";
+    private static final String BASE_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&limit=100";
     private EarthquakeAdapter earthquakeAdapter;
     private ListView earthquakeListView;
+    private TextView emptyStateTextView;
+    private NetworkInfo networkInfo;
     /**
      * Constant value for the earthquake loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
@@ -44,8 +51,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-        earthquakeListView = (ListView) findViewById(R.id.list_earthquakes);
 
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = connMgr.getActiveNetworkInfo();
+        //Initialize ListView
+        earthquakeListView = (ListView) findViewById(R.id.list_earthquakes);
+        //Initialize TextView for representing no data
+        emptyStateTextView = (TextView) findViewById(android.R.id.empty);
+        earthquakeListView.setEmptyView(emptyStateTextView);
         // Create a new {@link ArrayAdapter} of earthquakes
         earthquakeAdapter = new EarthquakeAdapter(getApplicationContext(), new ArrayList<Earthquake>());
 
@@ -65,26 +79,34 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
     @Override
     public void onLoadFinished(Loader<List<Earthquake>> loader, final List<Earthquake> data) {
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        if (networkInfo != null) {
+            progressBar.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.empty_data);
 
-        // Clear the adapter of previous earthquake data
-        earthquakeAdapter.clear();
+            // Clear the adapter of previous earthquake data
+            earthquakeAdapter.clear();
 
-        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-        // data set. This will trigger the ListView to update.
-        if (data != null && !data.isEmpty()) {
-            earthquakeAdapter.addAll(data);
-        }
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Earthquake earthquake = data.get(position);
-                Uri uri = Uri.parse(earthquake.getUrl());
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
+            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (data != null && !data.isEmpty()) {
+                earthquakeAdapter.addAll(data);
+                earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Earthquake earthquake = data.get(position);
+                        Uri uri = Uri.parse(earthquake.getUrl());
+                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
+                        }
+                    }
+                });
             }
-        });
+        } else {
+            progressBar.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet);
+        }
     }
 
     @Override
